0b561fac0e639191948ea12a59820393df3a2845
kgomez20 <59627719+kgomez20@users.noreply.github.com>
2020-02-05 16:37:16 -0700

    SchoolTranscriptDATA

--- DDL/SchoolTranscript.sql
+++ DDL/SchoolTranscript.sql
@@ -28,18 +28,22 @@ CREATE TABLE Students
     GivenName       varchar(50)     NOT NULL,
     Surname         varchar(50)     NOT NULL,
     DateOfBirth     datetime        NOT NULL,
-    Enrolled        bit             NOT NULL
+    Enrolled        bit
+        CONSTRAINT DF_Students_Enrolled
+            DEFAULT (1)             NOT NULL
 )
 
 CREATE TABLE Courses
 (
     Number          varChar(10)
         CONSTRAINT      PK_Courses_Number
-            PRIMARY KEY     NOT NULL,
+            PRIMARY KEY             NOT NULL,
     Name            varChar(50)     NOT NULL,
     Credits         decimal(3,1)    NOT NULL,
     Hours           tinyint         NOT NULL,
-    Active          bit             NOT NULL,
+    Active          bit
+        CONSTRAINT DF_Courses_Active
+            DEFAULT (1)             NOT NULL,
     Cost            money           NOT NULL
 )
 
@@ -51,7 +55,8 @@ CREATE TABLE StudentCourses
                                     NOT NULL,
     CourseNumber    varchar(10)
         CONSTRAINT FK_StudentCourses_CourseNumber_Courses_Number
-            FOREIGN KEY REFERENCES Courses(Number)     NOT NULL,
+            FOREIGN KEY REFERENCES Courses(Number)
+                                    NOT NULL,
     Year            tinyint         NOT NULL,
     Term            char(3)         NOT NULL,
     FinalMark       tinyint             NULL,
